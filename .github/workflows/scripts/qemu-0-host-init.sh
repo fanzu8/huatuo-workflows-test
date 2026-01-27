99ebb4eb254a6a88138972e4e03d7d6db0699a33
fanzu8 <tuzengbing@gmail.com>
2026-01-27 23:13:33 +0800

    test sim on amd64
    Signed-off-by: fanzu8 <tuzengbing@gmail.com>

--- .github/workflows/scripts/qemu-0-host-init.sh
+++ .github/workflows/scripts/qemu-0-host-init.sh
@@ -4,18 +4,29 @@ set -euo pipefail
 ARCH=${1:-amd64}
 OS_DISTRO=${2:-ubuntu24.04}
 
-# Handle different os distro
-case "$OS_DISTRO" in
-  ubuntu*)
-    # Install dependencies
-    sudo apt-get update -y
-    sudo apt-get install -y cloud-image-utils virt-manager qemu-utils qemu-system-arm
-    ;;
-#   centos*)
-#     # TODO:
-  *)
-    echo -e "❌ Unsupported OS distro: '$OS_DISTRO'" >&2
-    echo -e " Supported distros: ubuntu*" >&2
-    exit 1
-    ;;
+case "$ARCH" in
+amd64)
+	;;
+arm64)
+	;;
+*)
+	echo -e "❌ Unsupported ARCH: '$ARCH'" >&2
+	echo -e " Supported ARCHs: amd64, arm64" >&2
+	exit 1
+	;;
 esac
+
+
+case "$OS_DISTRO" in
+ubuntu*)
+	# Install dependencies
+	sudo apt-get update -y
+	sudo apt-get install -y cloud-image-utils virt-manager qemu-utils qemu-system-arm qemu-kvm
+	;;
+*)
+	echo -e "❌ Unsupported OS distro: '$OS_DISTRO'" >&2
+	echo -e " Supported distros: ubuntu*" >&2
+	exit 1
+	;;
+esac
+
